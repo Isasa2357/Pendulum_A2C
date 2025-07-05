b24d3497b6833999032458a8bc635d59fe57b15d
Isasa2357 <iwatake.s.b573@m.isct.ac.jp>
2025-07-06 03:12:06 +0900

    mutil_torchにsequentilのファクトリを追加

--- mutil_RL/mutil_torch.py
+++ mutil_RL/mutil_torch.py
@@ -2,6 +2,8 @@
     torch自作便利関数
 '''
 
+from typing import List
+
 import torch
 from torch import nn
 
@@ -11,13 +13,19 @@ def conv_str2ActivationFunc(module_str: str) -> nn.Module:
     '''
         文字列から活性化関数のnn.Moduleへ変換する
     '''
+    mapping = {
+        'ReLU': nn.ReLU(),
+        'Tanh': nn.Tanh(),
+        'Sigmoid': nn.Sigmoid(),
+        'Softplus': nn.Softplus(),
+        'Identity': nn.Identity()
+    }
+    if module_str in mapping:
+        return mapping[module_str]
+    else:
+        raise ValueError(f"Unknown activation function: {module_str}")
 
-    if module_str == 'ReLU':
-        return nn.ReLU()
-    
-    return nn.ReLU()
-
-def factory_LinearReLU_ModuleList(in_chnls: int, hdn_chnls: int, hdn_layers: int, out_chnls: int ,out_module: str=""):
+def factory_LinearReLU_ModuleList(in_chnls: int, hdn_chnls: int, hdn_layers: int, out_chnls: int ,out_act: str="") -> nn.ModuleList:
     '''
         LinearとReLUが積み重なったネットワークを作成
         最終層はカスタム可能
@@ -43,7 +51,22 @@ def factory_LinearReLU_ModuleList(in_chnls: int, hdn_chnls: int, hdn_layers: int
 
     # 出力層
     lays.append(nn.Linear(hdn_chnls, out_chnls))
-    if out_module != "":
-        lays.append(conv_str2ActivationFunc(out_module))
+    if out_act != "":
+        lays.append(conv_str2ActivationFunc(out_act))
 
     return lays
+
+def factory_LinearReLU_Sequential(in_chnls: int, hdn_chnls: int, hdn_layers: int, out_chnls: int ,out_act: str="") -> nn.Sequential:
+    '''
+        LinearとReLUが積み重なったネットワークを作成
+        最終層はカスタム可能
+
+        Args:
+            in_chnls: 入力チャネル
+            hdn_chnls: 隠れ層のチャネル数
+            hdn_lays: 隠れ層の層数
+            out_chnls: 出力層のチャネル数
+            out_module: 最終層の指定
+    '''
+    sequential = nn.Sequential(*factory_LinearReLU_ModuleList(in_chnls, hdn_chnls, hdn_layers, out_chnls, out_act))
+    return sequential
